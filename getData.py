3a2e319f0bed0bc531eaea017561c56cef9cf661
Malhar Patel <malhar@ualberta.ca>
2022-09-15 22:41:06 -0600

    RAW

--- getData.py
+++ getData.py
@@ -1,5 +1,5 @@
 import requests
 
-data = requests.get('https://www.google.com/')
+data = requests.get('https://raw.githubusercontent.com/malhar2000/CMPUT404-Lab1/master/getData.py')
 print(data)
 print(data.content)
